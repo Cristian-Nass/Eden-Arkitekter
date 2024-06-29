3bf91c0dcda58a9ff322887d7d58354312de92ba
Cristian Nass <cristian.tirdad@gmail.com>
2024-06-29 17:16:51 +0200

    add app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
     <>
+      <NavigationBar />
       <div>
         <h1>Eden Arkitekter</h1>
       </div>
