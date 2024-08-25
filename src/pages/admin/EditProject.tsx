393a9086cb462a7ce9510d599d0ef7f5d644948a
Cristian Nass <cristian.tirdad@gmail.com>
2024-08-25 13:37:36 +0200

    save

--- src/pages/admin/EditProject.tsx
+++ src/pages/admin/EditProject.tsx
@@ -1,7 +1,33 @@
+import { useDataStore } from "../../store/useDataStore";
+import { Divider } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+
 const EditProject = () => {
+  const { projects } = useDataStore();
+
   return (
     <>
-      <div style={{ paddingTop: "100px" }}>Edit Project</div>
+      {projects.map((project) => (
+        <div key={project.id}>
+          <Divider />
+          <div style={{ textAlign: "left", fontWeight: "bold" }}>
+            {project.title}
+          </div>
+          <div className='about-us-wrapper-txt'>{project.descriptions}</div>
+          <div style={{ textAlign: "right" }}>
+            <IconButton color='primary' aria-label='delete'>
+              <DeleteIcon />
+            </IconButton>
+
+            {/* <EditModal
+              title={abUs.title}
+              descriptions={abUs.descriptions}
+              id={abUs?.id}
+            /> */}
+          </div>
+        </div>
+      ))}
     </>
   );
 };
